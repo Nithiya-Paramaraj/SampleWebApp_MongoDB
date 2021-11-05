703b4c37a289d0e206b49abf2a2f36819c05a664
Nithiya-Paramaraj <91561518+Nithiya-Paramaraj@users.noreply.github.com>
2021-11-05 10:10:11 +0100

    Add files via upload
    First Commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Sample_Web_APP_MongoDB.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        
+        private IMongoCollection<Shipwreck> _shipwreckCollection;
+
+        public WeatherForecastController(IMongoClient client) //Dependency Injection in constructor
+        {
+            var database = client.GetDatabase("sample_geospatial");
+            _shipwreckCollection = database.GetCollection<Shipwreck>("shipwrecks");
+        }
+
+        [HttpGet]
+        public IEnumerable<Shipwreck> Get()
+        {                            
+            return _shipwreckCollection.Find(s => s.FeatureType == "Wrecks - Visible").ToList();
+
+        }
+    }
+}
---
+++ WeatherForecast.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sample_Web_APP_MongoDB
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
